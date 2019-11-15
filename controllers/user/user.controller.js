181a972a3f6b01f75e4054bed4dc11f23d3c1244
Quentin Kamenda <quentin.kamenda@isen.yncrea.fr>
2019-11-15 20:44:59 +0100

    patch user Controller

--- controllers/user/user.controller.js
+++ controllers/user/user.controller.js
@@ -66,7 +66,41 @@ export default class userController {
     * @returns {Object} return user before being modfied 
     */
     static async patch(req, res) {
+        const { id } = req.params
+        const userTargeted = await userService.getById(id)
+        const userConnected = req.user
 
+        // check if the user targeted for the profile picture exist
+        if (userTargeted == null) {
+            return res.status(HTTP_STATUS.NO_CONTENT).json({ error: 'User doesnt exist' })
+        }
+
+        // check if the user connected have the correct right to add the profile picture
+        if (userConnected.role.label === 'Admin' || userTargeted.id === userConnected.id) {
+            try {
+
+                // check the modifiable info
+                let { modifications } = req.body.modifications
+                
+                console.log("Modifications requested:" + req.body.modifications + "\n")
+                console.log("Modifications retrieved:" + modifications + "\n")
+
+                // updates the user information with the requested modifications
+                console.log("User before requested modifications:" + userTargeted + "\n")
+                await userService.update(userTargeted.id, modifications)
+
+                let userUpdated = await userService.getById(id)
+                onsole.log("User after requested modifications:" + userUpdated + "\n")
+
+                return res.status(HTTP_STATUS.OK).json({ status: 'user corectly updated' })
+            }
+            catch (error) {
+                logger.error(error)
+                return res.status(HTTP_STATUS.INTERNAL).json({ error: 'something went wrong while updating the user' })
+            }
+        }
+
+        return res.status(HTTP_STATUS.UNAUTHORIZED).json({ error: 'you dont have the correct right' })
     }
 
     /**
